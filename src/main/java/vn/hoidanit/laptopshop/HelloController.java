981283d73cc7e748c43893fdac790194cd5d2eb5
Long Tran <tranvanlong6677@gmail.com>
2024-03-11 22:34:11 +0700

    config

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -2,12 +2,24 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello world hihi";
+        return "Hello world                                                                                         ";
     }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "user hihi";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "admin hihi";
+    }
+
 }
--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,12 +2,23 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+// @SpringBootApplication
+// @SpringBootApplication(exclude =
+// org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class })
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
+		// SpringApplication.run(LaptopShopApplication.class, args);
+		@SuppressWarnings("unused")
+		ApplicationContext app = SpringApplication.run(LaptopshopApplication.class, args);
+		// for (String s : app.getBeanDefinitionNames()
+		// System.out.println(s);
+		// }
 	}
 
 }
